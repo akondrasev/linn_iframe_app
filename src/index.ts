9a8a568eb1a1bf9d610690ffba2eb06e9da4d729
Anton Kondrašev <anton.kondrasev@linnworks.com>
2023-11-21 12:08:19 +0200

    LinnworksApplication.init to get session

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+import { LinnworksApplication } from "@linnworks/application";
+
+LinnworksApplication.init()
+    .then(response => {
+        console.log(response.session);
+
+        let div = document.createElement("div");
+
+        for (let prop in response.session) {
+            let p = document.createElement("p");
+            let value = (response.session as any)[prop];
+            p.textContent = `${prop}: ${value}`;
+
+            div.appendChild(p);
+        }
+
+        document.body.appendChild(div);
+    })
+    .catch(() => {
+        alert(`Not connected to linnworks`);
+    });
