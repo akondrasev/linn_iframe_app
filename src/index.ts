023968f3a78473122f84d067600e1d4389305f56
Anton Kondrašev <anton.kondrasev@linnworks.com>
2024-04-15 16:11:35 +0300

    editorconfig

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 import { LinnworksApplication } from "@linnworks/application";
 
 LinnworksApplication.init()
-    .then(response => {
-        console.log(response.session);
-        console.log(response.initialData);
+  .then(response => {
+    console.log(response.session);
+    console.log(response.initialData);
 
-        let div = document.createElement("div");
+    let div = document.createElement("div");
 
-        for (let prop in response.session) {
-            let p = document.createElement("p");
-            let value = (response.session as any)[prop];
-            p.textContent = `${prop}: ${value}`;
+    for (let prop in response.session) {
+      let p = document.createElement("p");
+      let value = (response.session as any)[prop];
+      p.textContent = `${prop}: ${value}`;
 
-            div.appendChild(p);
-        }
+      div.appendChild(p);
+    }
 
-        document.body.appendChild(div);
-    })
-    .catch(() => {
-        alert(`Not connected to linnworks`);
-    });
+    document.body.appendChild(div);
+  })
+  .catch(() => {
+    alert(`Not connected to linnworks`);
+  });
