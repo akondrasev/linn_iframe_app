efc9cdbf4d003ee5d4a8c060270777a1867e883a
Anton Kondrašev <anton.kondrasev@linnworks.com>
2024-07-30 09:57:43 +0300

    clos, fetch examples

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,32 @@
 import { LinnworksApplication } from "@linnworks/application";
 
 LinnworksApplication.init()
-  .then(response => {
-    console.log(response.session);
-    console.log(response.initialData);
+  .then(({session, initialData, fetch, close}) => {
+    console.log(session);
+    console.log(initialData);
 
     let div = document.createElement("div");
 
-    for (let prop in response.session) {
+    for (let prop in session) {
       let p = document.createElement("p");
-      let value = (response.session as any)[prop];
+      let value = JSON.stringify((session as any)[prop]);
       p.textContent = `${prop}: ${value}`;
 
       div.appendChild(p);
     }
 
     document.body.appendChild(div);
+
+    var url = `/api/permissions/getPermissionsMetadata`;
+
+    fetch(url).then(response => {
+      console.dir(response);
+    });
+
+    document.body.querySelector("#close-btn")?.addEventListener("click", () => {
+      close()
+    });
   })
-  .catch(() => {
+  .catch((reason) => {
     alert(`Not connected to linnworks`);
   });
